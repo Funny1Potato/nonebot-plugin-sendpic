bc9e92bd7755e107979c7e3e31aa84c5940212c0
Funny1Potato <162463571+Funny1Potato@users.noreply.github.com>
2024-08-25 11:04:01 +0800

    Update config.py

--- nonebot_plugin_sendpic/config.py
+++ nonebot_plugin_sendpic/config.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     """Plugin Config Here"""
+    pic_num:list = []
+    pic_tag:list = []
+    pic_time:list = []
+    pic_group:list = []
