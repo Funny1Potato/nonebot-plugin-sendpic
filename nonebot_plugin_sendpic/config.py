64beedc9f2f2c29e4bc594bcf5224a526418e5c7
Funny1Potato <162463571+Funny1Potato@users.noreply.github.com>
2024-10-15 20:32:27 +0800

    Delete nonebot_plugin_sendpic directory

--- nonebot_plugin_sendpic/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    """Plugin Config Here"""
-    pic_num:list = []
-    pic_tag:list = []
-    pic_time:list = []
-    pic_group:list = []
